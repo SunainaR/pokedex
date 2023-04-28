a67065eefa10573d3670aa86c2fced555fb1a268
sunaina rishi <sunaina.d.rishi@gmail.com>
2023-04-28 12:35:58 +0100

    Corrected types and changed implict returns

--- api.js
+++ api.js
@@ -1,21 +1,33 @@
 
 
-
+const fetchPokemon = (pokemon_name) => {
+  return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon_name}`)
+  .then((response) => response.json())
+  .then((data) => { const pokemon 
+    = {
+    name: data.name,
+    id: data.id,
+    height: data.height,
+    weight: data.weight, 
+    types: data.types.map((type) => { return type.type.name})
+    }
+    return pokemon;
+  });
+};
 
 
 // const fetchPokemon = require('./api.js')
 //fetchPokemon('charizard').then((pokemon) => console.log(pokemon))
 
-const fetchPokemon = (pokemon_name) => {
-   return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon_name}`)
-  .then((response) => response.json())
-  .then((data) => pokemon = {
-    name: data.name,
-    id: data.id,
-    height: data.height,
-    weight: data.weight, 
-    types: data.types.map(type => type.name)
-  });
-};
 
 module.exports = fetchPokemon;
+
+
+/*
+const fetchPokemon = (pokemon_name) => {
+  return fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon_name}`)
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+};
+*/
+
